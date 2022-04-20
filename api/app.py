09bd693acaf5a02472a7ba4834b85d1cf5f95431
ahmadmustafaanis <ahmed.anis@redbuffer.net>
2022-04-20 12:29:21 +0500

    flask app

---
+++ api/app.py
@@ -0,0 +1,31 @@
+import flask
+from cfg import models as model_list
+from flask import jsonify, render_template, request
+from predict import Predictions
+
+models = model_list.models
+
+app = flask.Flask(__name__)
+app.config["debug"] = True
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", models=models)
+
+
+@app.route("/predict", methods=["POST"])
+def predict_gold():
+    model = Predictions(request.form.get("model_name"))
+    pred = model.predict(request.form.get("date"))
+    return jsonify(
+        {
+            "given_date": request.form.get("date"),
+            "next_date": model.get_next_date(),
+            "price": list(pred["trend"]),
+        },
+    )
+
+
+if __name__ == "__main__":
+    app.run()
